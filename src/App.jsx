9509e1f2b4007a333b3ce1f684f11024937548ef
amir <a.nouri1997@gmail.com>
2025-03-31 00:27:01 +0330

    initian GIT & making base of CMS project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Typography } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { DataGrid } from "@mui/x-data-grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+
+import "./App.css";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
