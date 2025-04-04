bd2fb6323540aaca31ac8dac30ff81fa6d3ecc10
amir <a.nouri1997@gmail.com>
2025-04-04 19:55:31 +0330

    create chart folder & component -> create datas file for it

---
+++ src/datas.jsx
@@ -0,0 +1,14 @@
+const xAxisData = [
+  { name: "Jan", sale: 112_000 },
+  { name: "Feb", sale: 99_000 },
+  { name: "Mar", sale: 77_000 },
+  { name: "Apr", sale: 85_000 },
+  { name: "May", sale: 87_700 },
+  { name: "Jun", sale: 110_500 },
+  { name: "Jul", sale: 120_550 },
+  { name: "Agu", sale: 11_440 },
+  { name: "Sep", sale: 19_200 },
+  { name: "Oct", sale: 56_000 },
+  { name: "Nov", sale: 66_666 },
+  { name: "Dec", sale: 112_112 },
+];
