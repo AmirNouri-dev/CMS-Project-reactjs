62f2972c0862c99bd289a9baccc00b91ad16bebc
amir <a.nouri1997@gmail.com>
2025-04-05 17:22:57 +0330

    userlist page coding & styling

--- src/pages/usersList/UsersList.jsx
+++ src/pages/usersList/UsersList.jsx
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import { userRows } from "./../../datas";
+import { DataGrid } from "@mui/x-data-grid";
+import { Link } from "react-router-dom";
+import "./UsersList.css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function UsersList() {
-  return <div>users</div>;
+  const [userDatas, setUserDatas] = useState(userRows);
+  const [paginationModel, setPaginationModel] = useState({
+    pageSize: 3,
+    page: 0,
+  });
+  function removeUser(userID) {
+    setUserDatas(
+      userDatas.filter((user) => {
+        return user.id != userID;
+      })
+    );
+  }
+
+  const columns = [
+    { field: "id", headerName: "id", width: 90 },
+
+    {
+      field: "user",
+      headerName: "user",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <Link to="/" className="link">
+            <div className="userList-User">
+              <img
+                src={params.row.avatar}
+                alt="avatar"
+                className="userList-User_img"
+              />
+              {params.row.userName}
+            </div>
+          </Link>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
+    { field: "status", headerName: "Status", width: 120 },
+    { field: "transAction", headerName: "Transaction", width: 120 },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <div className="userList-User_action">
+            <Link to={`/user/${params.row.id}`}>
+              <button className="userList-User-edit-btn">Edit</button>
+            </Link>
+            <DeleteIcon
+              className="userList-User-delete-btn"
+              onClick={() => removeUser(params.row.id)}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="userList">
+      <DataGrid
+        className="userList-dataGrid"
+        rows={userDatas}
+        columns={columns}
+        paginationModel={paginationModel}
+        onPaginationModelChange={setPaginationModel}
+        pageSizeOptions={[2, 3, 5, 10]}
+        row
+      />
+    </div>
+  );
 }
