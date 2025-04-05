d36407cde2073bad386a8baadb695653df0f9176
amir <a.nouri1997@gmail.com>
2025-04-05 18:42:29 +0330

    code & style product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
 export default function Product() {
-  return <div></div>;
+  return (
+    <div className="product">
+      <div className="product-Title_container">
+        <h1 className="product-Title">Product</h1>
+        <Link>
+          <button className="product-Title_add-btn">Create</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
