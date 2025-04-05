2d953682156851d65dcd7f4e3ea82b0f4e79da71
amir <a.nouri1997@gmail.com>
2025-04-05 12:23:57 +0330

    coding widgetLg component

--- src/components/widgetLg/WidgetLg.jsx
+++ src/components/widgetLg/WidgetLg.jsx
@@ -1,5 +1,68 @@
 import React from "react";
 import "./WidgetLg.css";
 export default function WidgetLg() {
-  return <div>large</div>;
+  const Button = ({ type }) => {
+    return <button className={"widgetLg-button " + type}>{type}</button>;
+  };
+  return (
+    <div className="widgetLg">
+      <div className="widgetLg-title">Latest TransActions</div>
+      <table className="widgetLg-table">
+        <tr className="widgetLg-tr">
+          <th className="widgetLg-th">Customer</th>
+          <th className="widgetLg-th">Date</th>
+          <th className="widgetLg-th">Amount</th>
+          <th className="widgetLg-th">Status</th>
+        </tr>
+        {/* approvedbtn */}
+        <tr className="widgetLg-tr">
+          <td className="widgetLg-user">
+            <img
+              src="./../../public/images/avatar.png"
+              alt=""
+              className="widgetLg-user_img"
+            />
+            <span className="widgetLg-user_name">Omid Rohani</span>
+          </td>
+          <td className="widgetLg-date">20 March 2025</td>
+          <td className="widgetLg-amount">$199.99</td>
+          <td className="widgetLg-status">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        {/* declinebtn */}
+        <tr className="widgetLg-tr">
+          <td className="widgetLg-user">
+            <img
+              src="./../../public/images/avatar.png"
+              alt=""
+              className="widgetLg-user_img"
+            />
+            <span className="widgetLg-user_name">Omid Rohani</span>
+          </td>
+          <td className="widgetLg-date">20 March 2025</td>
+          <td className="widgetLg-amount">$199.99</td>
+          <td className="widgetLg-status">
+            <Button type="Decline" />
+          </td>
+        </tr>
+        {/* pendingbtn */}
+        <tr className="widgetLg-tr">
+          <td className="widgetLg-user">
+            <img
+              src="./../../public/images/avatar.png"
+              alt=""
+              className="widgetLg-user_img"
+            />
+            <span className="widgetLg-user_name">Omid Rohani</span>
+          </td>
+          <td className="widgetLg-date">20 March 2025</td>
+          <td className="widgetLg-amount">$199.99</td>
+          <td className="widgetLg-status">
+            <Button type="Pending" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
